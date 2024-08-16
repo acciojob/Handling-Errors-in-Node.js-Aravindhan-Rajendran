d24843fbadf8d77d9d376d395d5453d1cbd8cdf6
Aravindhan-Rajendran <aravindhan12072000@gmail.com>
2024-08-16 16:45:06 +0530

    d

--- main.js
+++ main.js
@@ -17,17 +17,18 @@ function readFileContent(filePath) {
   // Resolve the file path to ensure it’s absolute
   const resolvedPath = path.resolve(filePath);
 
+  columnname=path.basename(filePath);
   // Attempt to read the file
   fs.readFile(resolvedPath, 'utf8', (err, data) => {
     if (err) {
       if (err.code === 'ENOENT') {
-        console.error(`Error: Column '${path.basename(filePath)}' not found in the CSV.`);
+        console.error(`Error: Column '${columnname}' not found in the CSV.`);
       } else {
         console.error("Error: Unable to read the file.");
       }
     } else {
       console.log(data);
-    }
+    }g
   });
 }
 
